132febae9d36850c4b642f62f843308d30954aeb
lffteclado <lffteclado@gmail.com>
2017-03-06 22:13:02 -0300

    Primeiro Commit, busca de paises funcionando.

---
+++ BuscaPaises/BuscaPaises/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BuscaPaises
+{
+    public partial class App : Application
+    {
+        private MainPage _mainPage;
+        public App()
+        {
+            _mainPage = new BuscaPaises.MainPage();
+
+            InitializeComponent();
+
+            MainPage = _mainPage;
+
+            //MainPage = new BuscaPaises.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            _mainPage.Load();
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ BuscaPaises/BuscaPaises/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaPaises
+{ 
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string SubRegion { get; set; }
+    }
+}
