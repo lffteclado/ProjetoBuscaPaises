6514b61b4b61cd863e87dbb5abe93bbd825cb3c6
lffteclado <lffteclado@gmail.com>
2017-03-07 22:18:00 -0300

    Criação das Classes

--- BuscaPaises/BuscaPaises/Pages/MainPage.xaml.cs
+++ BuscaPaises/BuscaPaises/Pages/MainPage.xaml.cs
@@ -5,37 +5,73 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace BuscaPaises
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
-        public ObservableCollection<Country> Countries { get; set; }
-        private RestClient _client;
-        public Command RefreshCommand { get; set; }
+        protected CountryService DataService { get; set; }
+
+        public ObservableCollection<CountryViewModel> Countries { get; set; }
+        public ICommand CallCommand { get; set; }
+        public ICommand StatusCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public NavigationService NavigationService { get; set; }
+
 
         public MainPage()
         {
-            RefreshCommand = new Command(() => Load());
-            Countries = new ObservableCollection<Country>();
-            _client = new RestClient();
+            Countries = new ObservableCollection<CountryViewModel>();
+            DataService = new CountryService();
+            CallCommand = new Command (obj => CallCountries());
+            StatusCommand = new Command(obj => CallPlain());
+            SearchCommand = new Command(obj=> CallSearch());
+            NavigationService = new NavigationService(Navigation);
 
             InitializeComponent();
+
+            List.ItemTapped += (sender, e) =>
+            {
+                var viewModel = ((ListView)sender).BindingContext as CountryViewModel;
+
+                if (viewModel != null && viewModel.BrowseCommand != null)
+                {
+                    viewModel.BrowseCommand.Execute(((ListView)sender).BindingContext);
+                }
+            };
         }
 
-        public async void Load()
+        protected override void OnAppearing()
         {
-            var result = await _client.GetCountries();
+            base.OnAppearing();
 
-            Countries.Clear();
-
-            foreach (var item in result)
+            if (Countries.Any())
             {
-                Countries.Add(item);
+                return;
             }
 
-            IsBusy = false;
+            CallCountries();
+        }
+
+        private async void CallPlain()
+        {
+            CallButton.Text = "Carregando...";
+            IsBusy = true;
+            List.IsVisible = true;
+
+            try
+            {
+                Response.Text = await DataService.GetData();
+                List.IsVisible = false;
+                StatusPanel.IsVisible = true;
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
     }
+
 }
